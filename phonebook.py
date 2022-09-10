c9cc55bf1f37aacb64650d29a77acbcfcca9c277
itehnolog <itehnolog@ukr.net>
2022-09-10 17:10:56 +0300

    coments and some changes added

--- phonebook.py
+++ phonebook.py
@@ -1,6 +1,7 @@
 from collections import UserDict
 from datetime import datetime
 import re
+from typing import List, Optional
 
 import errors
 
@@ -78,44 +79,97 @@ class Phone(Field):
 
 
 class Record:
+    """
+    Contact record
+    Attributes: name, birthday, emails,  phones, birthday, emails
+    """
 
-    def __init__(self, name: Name, phones: Phone = [], birthday: Birthday = None, emails=[]):
+    def __init__(self, name: Name, phones: Optional[List[Phone]] = None,
+                 birthday: Optional[Birthday] = None, emails: Optional[List[Email]] = None) -> None:
         self.name = name
-        self.phones = [phones] if phones is not None else[]
+        self.phones = phones
         self.birthday = birthday
-        self.emails = [emails] if emails is not None else[]
+        self.emails = emails
 
-    def add_phone(self, phone):
-        self.phones.append(phone)
+    def add_phone(self, phone: Phone) -> None:
+        """
+        Adding new phone to current record
+        :param phone: instance of Phone
+        :return: None
+        """
+        if self.phones is None:
+            self.phones = [phone]
+        else:
+            self.phones.append(phone)
+        return "Phone number added"
 
-    def change_phone(self, old_phone, new_phone):
-        try:
-            self.phones.remove(old_phone)
-            self.phones.append(new_phone)
-        except ValueError:
-            return f"{old_phone} does not exist"
+    def change_phone(self, old_phone: str, new_phone: Phone) -> str | None:
+        """
+        Changing user phone
+        :param old_phone: phone to be changed
+        :param new_phone: instance of Phone
+        :return: string if no phone to be changed was found else None
+        """
+        for phone in self.phones:
+            if phone.value == old_phone:
+                self.phones.remove(phone.value)
+                self.phones.append(new_phone)
+                return "Phone number changed"
+            else:
+                return f"{old_phone} does not exist"
 
-    def delete_phone(self, phone):
-        try:
-            self.phones.remove(phone)
-        except ValueError:
-            return f"{phone} does not exists"
+    def delete_phone(self, phone_to_delete: str) -> str | None:
+        """
+        Deleting phone
+        :param phone_to_delete: phone to be deleted
+        :return: string if no phone was found else None
+        """
+        for phone in self.phones:
+            if phone.value == phone_to_delete:
+                self.phones.remove(phone.value)
+                return "Phone number deleted"
+            else:
+                return f"{phone_to_delete} does not exist"
 
-    def add_email(self, email):
-        self.emails.append(email)
+    def add_email(self, email: Email) -> None:
+        """
+        Adds new email to current record
+        :param email: instance of Email
+        :return: None
+        """
+        if self.emails is None:
+            self.emails = [email]
+        else:
+            self.emails.append(email)
+        return "Email added"
 
-    def change_email(self, old_email, new_email):
-        try:
-            self.emails.remove(old_email)
-            self.emails.append(new_email)
-        except ValueError:
+    def change_email(self, old_email: str, new_email: Email) -> str | None:
+        """
+        Changing user email
+        :param old_email: email to be changed
+        :param new_email: instance of Email
+        :return: string if no email to be changed was found else None
+        """
+        for email in self.emails:
+            if email.value == old_email:
+                self.emails.remove(email.value)
+                self.emails.append(new_email)
+                return "Email changed"
+        else:
             return f"{old_email} does not exist"
 
-    def delete_email(self, email):
-        try:
-            self.emails.remove(email)
-        except ValueError:
-            return f"{email} does not exists"
+    def delete_email(self, email_to_delete: str) -> str | None:
+        """
+        Deleting email
+        :param email_to_delete: email to be deleted
+        :return: string if no email was found else None
+        """
+        for email in self.emails:
+            if email.value == email_to_delete:
+                self.emails.remove(email.value)
+                return "Email deleted"
+        else:
+            return f"{email_to_delete} does not exist"
 
 
 class AddressBook(UserDict):
