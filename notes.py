bc4d5bedaf76f7e0abedf6045f733eb2edd8bfbb
Volodymyr Suprunets <raraot@gmail.com>
2022-09-12 10:59:22 -0600

    added search (it remains to do sorting)

--- notes.py
+++ notes.py
@@ -1,95 +1,108 @@
 from collections import UserDict
 from datetime import datetime
-import csv
+import pickle
 
 
 class NotesBook(UserDict):
     def __init__(self):
-        self.filename = "notesbook.csv"
+        self.filename = "notesbook.txt"
         self.data = {}
 
-    def read_file(self):
+    def read_file(self) -> None:
         try:
-            with open(self.filename, 'r') as fh:
-                reader = csv.DictReader(fh, delimiter=',')
-                for row in reader:
-                    self.data[row['tag']]={'create': row['create'], 'note': row['note']}
+            with open(self.filename, 'rb') as fh:
+                unpacked = pickle.load(fh)
+                self.data = unpacked
         except FileNotFoundError:
             self.data = {}
 
-    def save_in_file(self):
-        with open(self.filename, 'w') as fh:
-            header_names = ['tag', 'create', 'note']
-            writer = csv.DictWriter(fh, fieldnames = header_names, delimiter=',')
-            writer.writeheader()
-            for k, v in self.data.items():
-                writer.writerow({'tag': k, 'create': v['create'], 'note': v['note']})   
+    def save_in_file(self) -> None:
+        with open(self.filename, 'wb') as fh:
+            pickle.dump(self.data, fh)
+
 
 class NotesCommands(NotesBook):
 
-    def add_note(self, tag: str, note: str, notesbook1: NotesBook) -> str:
-        notesbook1.read_file()
+    def add_note(self, name: str, tags: str, note: str, notesbook: NotesBook) -> str:
+        notesbook.read_file()
         create = datetime.now()
-        if tag not in notesbook1.data:   
-            all_note_data = {"create": create.strftime("%d.%m.%Y %H:%M"), "note": note}
-            notesbook1.data[tag] = all_note_data
-            notesbook1.save_in_file()
-            return '** note added successfully **'
+        if name not in notesbook.data:   
+            list_tags = []
+            for i in tags.replace('.', ' ').replace(',', ' ').replace(';', ' ').replace('  ', ' ').split(' '):
+                list_tags.append(i)
+            note_data = {"tags": list_tags, "create": create.strftime("%d.%m.%Y %H:%M"), "note": note}
+            notesbook.data[name] = note_data
+            notesbook.save_in_file()
         else:
-            print(f'\033[33mnote with name \033[43m {input_tag} \033[0m\033[33m exists, if you want to change it enter the command: edit note\033[37m')
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m exists, if you want to change it enter the command: edit note\033[37m')
 
+    def delete_note(self, name: str, notesbook: NotesBook) -> None:
+        notesbook.read_file()
+        if name not in notesbook.data:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
+        else:
+            del notesbook.data[name]
+            notesbook.save_in_file()
 
-    def delete_note(self, tag: str, notesbook1: NotesBook):
-        notesbook1.read_file()
-        if input_tag not in notesbook1.data:
-            print(f'\033[33mnote with name \033[43m {input_tag} \033[0m\033[33m does not find\033[0m')
+    def edit_note(self, name: str, note: str, notesbook: NotesBook) -> None:
+        notesbook.read_file()
+        if name not in notesbook.data:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
         else:
-            del notesbook1.data[tag]
-            notesbook1.save_in_file()
-            return f'** note {tag} delete successfully **'
+            notesbook.data[name]["note"] = note
+            notesbook.save_in_file()
 
-    def edit_note(self, tag: str, note: str, notesbook1: NotesBook):
-        notesbook1.read_file()
-        if tag not in notesbook1.data:
-            print(f'\033[33mnote with name \033[43m {tag} \033[0m\033[33m does not find\033[0m')
+    def text_note(self, name: str,  notesbook: NotesBook) -> str:
+        notesbook.read_file()
+        return notesbook.data[name]["note"]
+
+    def edit_tags(self, name: str, tags: str, notesbook: NotesBook) -> None:
+        notesbook.read_file()
+        if name not in notesbook.data:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
         else:
-            notesbook1.data[tag]["note"] = note
-            notesbook1.save_in_file()
-            return f'** note {tag} edit successfully **'
-    
-    def show_some_note(self, tag: str,  notesbook1: NotesBook):
-        notesbook1.read_file()
-        if tag not in notesbook1.data:
-            print(f'\033[33mnote with name \033[43m {tag} \033[0m\033[33m does not find\033[0m')
+            list_tags = []
+            for i in tags.replace('.', ' ').replace(',', ' ').replace(';', ' ').replace('  ', ' ').split(' '):
+                list_tags.append(i)
+            notesbook.data[name]["tags"] = list_tags
+            notesbook.save_in_file()
+
+    def text_tags(self, name: str,  notesbook: NotesBook) -> str:
+        notesbook.read_file()
+        return notesbook.data[name]["tags"]    
+
+    def show_some_note(self, name: str,  notesbook: NotesBook) -> None:
+        notesbook.read_file()
+        if name not in notesbook.data:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
         else:
-            result = f'''date create note: {notesbook1.data[tag]["create"]}\nnote text: {notesbook1.data[tag]["note"]}\n'''
+            result = f'''\n  name:  {name}\n  tags:  {", ".join(notesbook.data[name]["tags"])}\ncreate:  {notesbook.data[name]["create"]}\n  note:  {notesbook.data[name]["note"]}\n'''
             print(result)
-            return result
 
-    def show_all_notes(self, notesbook1: NotesBook):
-        notesbook1.read_file()
-        result = "\033[32m\n*** ALL YOUR NOTES ***\033[0m\n\n"
-        for k, v in notesbook1.data.items():
-            result += f'''tag: {k}\ncreate: {v["create"]}\nnote: {v["note"]}\n\n'''
+    def show_all_notes(self, notesbook: NotesBook) -> None:
+        notesbook.read_file()
+        result = "\033[32m\n*** ALL YOUR NOTES ***\033[0m\n"
+        for k, v in notesbook.data.items():
+            result += f'''\n  name:  {k}\n  tags:  {", ".join(v["tags"])}\ncreate:  {v["create"]}\n  note:  {v["note"]}\n'''
         print(result)
-        return result
 
-    def find_note(self, request: str, notesbook1: NotesBook):
-        notesbook1.read_file()
+    def find_note(self, request: str, notesbook: NotesBook) -> None:
+        notesbook.read_file()
         find = ""
-        for k, v in notesbook1.data.items():
-            if request in k:
-                find += f'''tag: {k}\ncreate: {v["create"]}\nnote: {v["note"]}\n\n'''
+        for k, v in notesbook.data.items():
+            if (request.lower() in k.lower()) or (request.lower() in v["note"].lower()):
+                find += f'''\n  name:  {k}\n  tags:  {", ".join(v["tags"])}\ncreate:  {v["create"]}\n  note:  {v["note"]}\n'''
         if find == "":
-            print('\033[33m*** No notes were found matching your request ***\033[0m')
+            print('\033[33m*** No notes found for your request ***\033[0m')
         else:
-            result = "\033[32m\n*** FIND NEXT NOTES ***\n\n\033[0m" + find
+            result = "\033[32m\n*** FIND NEXT NOTES ***\n\033[0m" + find
             print(result)
-            return result
-        
-# *** next need deleted ***
+
+    def sort(self, select: str, notesbook: NotesBook) -> None:
+        pass
+
+
 # НАСТУПНИЙ КОД ТІЛЬКИ ДЛЯ ПЕРЕВІРКИ ОКРЕМОГО ФУНКЦІОНУВАННЯ РОБОТИ З НОТАТКАМИ (ВИДАЛИТИ ПРИ ІНТЕГРУВАННІ)
-# mini handler only for test
 
 notescommands = NotesCommands()
 
@@ -105,28 +118,49 @@ stop - вихід з циклу Бота
 print('\033[32m\n*** start a mini CLI-bot (only for test notes) ***\n\033[0m')
 print(welcome)
 while True:
+
     userinput = input(": ")
     if userinput.lstrip().lower() == "stop":
         print('\033[32m*** mini CLI-bot end of work (only for test notes) ***\n\033[0m')
         break
+
     elif userinput.lstrip().lower() == "new note":
-        input_tag = input(": enter note name: ")
-        input_note_text = input(": enter note text: ")
-        notescommands.add_note(input_tag, input_note_text, NotesBook())
+        name = input(": enter note name: ")
+        tags = input(": enter tags for note: ")
+        text = input(": enter note text: ")
+        notescommands.add_note(name, tags, text, NotesBook())
+
     elif userinput.lstrip()[0:11].lower() == "delete note":
-        input_tag = input(": name of the note you want to delete: ")
+        name = input(": name of the note you want to delete: ")
         confirm = input(": do you really want to delete y/n: ")
         if confirm.lower() == "y":
-            notescommands.delete_note(input_tag, NotesBook())
+            notescommands.delete_note(name, NotesBook())
+
     elif userinput.lstrip()[0:9].lower() == "edit note":
-        input_tag = input(": name of the note you want to edit: ")
-        new_note = input(": enter new text for note: ")
-        notescommands.edit_note(input_tag, new_note, NotesBook())
+        name = input(": name of the note you want to edit: ")
+        try:
+            print(f": old  text note: {notescommands.text_note(name, NotesBook())}")
+            new_note = input(": edit text note: ")
+            notescommands.edit_note(name, new_note, NotesBook())
+        except KeyError:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
+
+    elif userinput.lstrip()[0:8].lower() == "edit tag":
+        name = input(": name of the note where you want to edit tags: ")
+        try:
+            print(f": old  tags: {', '.join(notescommands.text_tags(name, NotesBook()))}")
+            new_tags = input(": edit tags: ")
+            notescommands.edit_tags(name, new_tags, NotesBook())
+        except KeyError:
+            print(f'\033[33mnote with name \033[43m {name} \033[0m\033[33m does not find\033[0m')
+
     elif (userinput.lstrip().lower() == "all notes") or (userinput.lstrip().lower() == "all note"):
         notescommands.show_all_notes(NotesBook())
+
     elif userinput.lstrip().lower() == "note":
-        input_tag = input(": enter the name of the note you want to view: ")
-        notescommands.show_some_note(input_tag, NotesBook())
+        name = input(": enter the name of the note you want to view: ")
+        notescommands.show_some_note(name, NotesBook())
+
     elif userinput.lstrip().lower() == "find note":
         request = input(": enter a query to search: ")
         notescommands.find_note(request, NotesBook())       
@@ -134,25 +168,7 @@ while True:
 
 # python3 notes.py
 
-# залишилось зробити теги окремо ллістом з сортуванням і відображенням по тегам
 # init open read file when start
-# зберігати нотатки з текстовою інформацією;
-# проводити пошук за нотатками; - пошук зробити по всі полям крім тегів
-# редагувати та видаляти нотатки;
-# додавати в нотатки "теги", ключові слова, що описують тему та предмет запису;
-# здійснювати пошук та сортування нотаток за ключовими словами (тегами);
-# красивое форматирование отображения всех заметок
-# add_note(tag: str, note: str)
-# delete_note(tag: str)
-# edit_note(tag: str, note: str)
+# пошук та сортування нотаток за ключовими словами (тегами);
+# сортування по дате
 # описати всі def
-
-#   name:  love music
-#   tags:  song, music
-# create:  '10.09.2022 18:30'
-#   note:  ac-dc beatles brainstorm avril lavine
-
-# tag: newmusic 1
-# create: 10.09.2022 19:26
-# note: rty uio fgh nmj
-
